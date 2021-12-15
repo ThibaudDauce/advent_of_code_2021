5e43e54b8df2b360d2daa147e2effb40b7cbf24f
Thibaud Dauce <git@hadibut.fr>
2021-12-15 12:09:39 +0100

    Day 14 - Part 2

--- src/14.rs
+++ src/14.rs
@@ -3,10 +3,11 @@ use std::collections::HashMap;
 fn main()
 {
     part1();
+    part2();
 }
 
 type Pair = (char, char);
-type Rules = HashMap<Pair, HashMap<u32, Vec<Pair>>>;
+type Rules = HashMap<Pair, HashMap<u64, Vec<Pair>>>;
 
 fn part1()
 {
@@ -14,12 +15,19 @@ fn part1()
     println!("Part 1: {}", result);
 }
 
-fn compute_string(input: &'static str, steps: u32) -> String
+fn part2()
+{
+    let result = compute_result(raw_input(), 40);
+    println!("Part 2: {}", result);
+}
+
+fn compute(input: &'static str, steps: u64) -> (Rules, Vec<Pair>)
 {
     let (template, mut rules) = build_input(input);
 
     let mut new_pairs = vec![];
     for pair in template {
+        // println!("{}{}", pair.0, pair.1);
         let result = get_result(rules, pair, steps);
 
         rules = result.0;
@@ -28,18 +36,42 @@ fn compute_string(input: &'static str, steps: u32) -> String
         }
     }
 
-    pairs_to_string(new_pairs)
+    (rules, new_pairs)
 }
 
-fn compute_result(input: &'static str, steps: u32) -> u32
+fn compute_result(input: &'static str, steps: u64) -> u64
 {
-    let mut chars_counts: HashMap<char, u32> = HashMap::new();
+    println!("Computing {} steps…", steps / 2);
+    let (mut rules, pairs) = compute(input, steps / 2);
 
-    let result = compute_string(input, steps);
+    let mut chars_counts: HashMap<char, u64> = HashMap::new();
+    let mut first = true;
 
-    for char in result.chars() {
-        let entry = chars_counts.entry(char).or_insert(0);
-        *entry += 1;
+    let mut chars_counts_by_pair: HashMap<Pair, (Vec<Pair>, HashMap<char, u64>)> = HashMap::new();
+    for (index, pair) in pairs.iter().enumerate() {
+        if ! chars_counts_by_pair.contains_key(pair) {
+            println!("\tComputing count for {}{} ({}/{})", pair.0, pair.1, index, pairs.len());
+    
+            let result = get_result(rules, *pair, steps / 2);
+            let this_pair_chars_counts = compute_counts(&result.1);
+
+            rules = result.0;
+            chars_counts_by_pair.insert(*pair, (result.1, this_pair_chars_counts));
+        }
+
+        let (this_pair_pairs, this_pair_chars_counts) = chars_counts_by_pair.get(pair).unwrap();
+
+        for (char, count) in this_pair_chars_counts {
+            let entry = chars_counts.entry(*char).or_insert(0);
+            *entry += count;
+        }
+        if first {
+            first = false;
+        } else {
+            let count = chars_counts.get_mut(&this_pair_pairs.first().unwrap().0).unwrap();
+            *count -= 1;
+        }
+
     }
 
     let min = chars_counts.values().min().unwrap();
@@ -48,33 +80,27 @@ fn compute_result(input: &'static str, steps: u32) -> u32
     max - min
 }
 
-fn build_input(input: &'static str) -> (Vec<Pair>, Rules)
+fn compute_counts(pairs: &Vec<Pair>) -> HashMap<char, u64>
 {
-    let (template_as_string, rules_as_string) = input.trim().split_once("\n\n").unwrap();
+    let mut chars_counts = HashMap::new();
 
-    let mut rules: Rules = HashMap::new();
-    for line in rules_as_string.lines().map(|line| line.trim()) {
-        let (from_as_string, to_as_string) = line.split_once(" -> ").unwrap();
+    let mut first = true;
+    for (a, b) in pairs {
+        if first {
+            let entry = chars_counts.entry(*a).or_insert(0);
+            *entry += 1;
+            first = false;
+        }
 
-        let mut from_chars = from_as_string.chars();
-        let from_a = from_chars.next().unwrap();
-        let from_b = from_chars.next().unwrap();
-        let from: Pair = (from_a, from_b);
-        let to: char = to_as_string.chars().next().unwrap();
-
-        let entry = rules.entry(from).or_insert_with(HashMap::new);
-        entry.insert(1, vec![(from_a, to), (to, from_b)]);
+        let entry = chars_counts.entry(*b).or_insert(0);
+        *entry += 1;
     }
 
-
-    let chars: Vec<char> = template_as_string.chars().collect();
-
-    let template: Vec<Pair> = chars.windows(2).map(|digits| (digits[0], digits[1])).collect();
-
-    (template, rules)
+    chars_counts
 }
 
-fn get_result(mut rules: Rules, pair: Pair, steps: u32) -> (Rules, Vec<Pair>)
+
+fn get_result(mut rules: Rules, pair: Pair, steps: u64) -> (Rules, Vec<Pair>)
 {
     let instructions = rules.get(&pair).unwrap();
 
@@ -101,6 +127,9 @@ fn get_result(mut rules: Rules, pair: Pair, steps: u32) -> (Rules, Vec<Pair>)
         }
     }
 
+    let instructions = rules.get_mut(&pair).unwrap();
+    instructions.insert(steps, new_pairs.clone());
+
     (rules, new_pairs)
 }
 
@@ -122,11 +151,17 @@ fn test_part1()
     let result = get_result(build_input(test_input()).1, ('N', 'N'), 4);
     assert_eq!("NBBNBNBBCCNBCNCCN", pairs_to_string(result.1));
 
-    let result = compute_string(test_input(), 4);
+    let result = pairs_to_string(compute(test_input(), 4).1);
     assert_eq!("NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB", result);
 
     let result = compute_result(test_input(), 10);
     assert_eq!(1588, result);
+
+    let result = compute_result(raw_input(), 10);
+    assert_eq!(2360, result);
+
+    let result = compute_result(test_input(), 40);
+    assert_eq!(2188189693529, result);
 }
 
 fn pairs_to_string(pairs: Vec<Pair>) -> String
@@ -144,6 +179,32 @@ fn pairs_to_string(pairs: Vec<Pair>) -> String
     }
 
     s
+}
+
+fn build_input(input: &'static str) -> (Vec<Pair>, Rules)
+{
+    let (template_as_string, rules_as_string) = input.trim().split_once("\n\n").unwrap();
+
+    let mut rules: Rules = HashMap::new();
+    for line in rules_as_string.lines().map(|line| line.trim()) {
+        let (from_as_string, to_as_string) = line.split_once(" -> ").unwrap();
+
+        let mut from_chars = from_as_string.chars();
+        let from_a = from_chars.next().unwrap();
+        let from_b = from_chars.next().unwrap();
+        let from: Pair = (from_a, from_b);
+        let to: char = to_as_string.chars().next().unwrap();
+
+        let entry = rules.entry(from).or_insert_with(HashMap::new);
+        entry.insert(1, vec![(from_a, to), (to, from_b)]);
+    }
+
+
+    let chars: Vec<char> = template_as_string.chars().collect();
+
+    let template: Vec<Pair> = chars.windows(2).map(|digits| (digits[0], digits[1])).collect();
+
+    (template, rules)
 }
 
 fn test_input() -> &'static str
