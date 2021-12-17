5838d98c2922c669fc6c9a8922381e166eb1c130
Thibaud Dauce <git@hadibut.fr>
2021-12-17 12:01:27 +0100

    Day 17 - Part 1

---
+++ src/17.rs
@@ -0,0 +1,63 @@
+fn main()
+{
+    // println!("Part 1: {}", part1(20, 30, -10, -5));
+
+    println!("Part 1: {}", part1(150, 193, -136, -86));
+}
+
+fn part1(min_x: i32, max_x: i32, min_y: i32, max_y: i32) -> i32
+{
+    assert!(min_x < max_x);
+    assert!(min_y < max_y);
+
+    assert!(min_x > 0);
+    assert!(max_y < 0);
+
+    let mut max_y_reached = i32::MIN;
+
+    for initial_x in 1..max_x + 1 + 5 {
+        for initial_y in min_y..(min_y * -1 + 5) {
+
+            let mut acc_x = initial_x;
+            let mut acc_y = initial_y;
+            let mut x = 0;
+            let mut y = 0;
+            let mut max_y_reached_this_time = i32::MIN;
+
+            loop {
+                x += acc_x;
+                y += acc_y;
+
+                if y > max_y_reached_this_time {
+                    max_y_reached_this_time = y;
+                }
+
+                if acc_x > 0 {
+                    acc_x -= 1;
+                } else if acc_x < 0 {
+                    acc_x += 1;
+                }
+
+                acc_y -= 1;
+
+                if x >= min_x && x <= max_x && y >= min_y && y <= max_y {
+
+                    if max_y_reached_this_time > max_y_reached {
+                        max_y_reached = max_y_reached_this_time;
+                    }
+                    
+                    break;
+                }
+
+                if x > max_x {
+                    break;
+                }
+                if y < min_y {
+                    break;
+                }
+            }
+        }
+    }
+
+    max_y_reached
+}
