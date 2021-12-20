50dc732bd54d0e9f96ca40d12bf1fdbf5570cc5b
Thibaud Dauce <git@hadibut.fr>
2021-12-20 09:49:08 +0100

    Day 18 - Part 2

--- src/18.rs
+++ src/18.rs
@@ -1,6 +1,33 @@
 fn main()
 {
     println!("Part 1: {}", magnitude(add_lines(raw_input())));
+    println!("Part 2: {}", max_magnitude(raw_input()));
+}
+
+fn max_magnitude(input: &'static str) -> u32
+{
+    let numbers: Vec<Number> = input.trim().lines().map(|line| parse(line.trim())).collect();
+    
+
+    let mut max = 0;
+
+    for i in 0..numbers.len() {
+        for j in 0..numbers.len() {
+            if i == j {
+                continue;
+            }
+
+            let a = numbers[i].clone();
+            let b = numbers[j].clone();
+
+            let magnitude = magnitude(add_and_reduce(a, b));
+            if magnitude > max {
+                max = magnitude;
+            }
+        }
+    }
+
+    max
 }
 
 fn add_lines(input: &'static str) -> Number
@@ -256,6 +283,18 @@ fn it_works()
     assert_eq!(1137, magnitude(parse("[[[[5,0],[7,4]],[5,5]],[6,6]]")));
     assert_eq!(3488, magnitude(parse("[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]")));
 
+    assert_eq!(3993, max_magnitude("
+        [[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]
+        [[[5,[2,8]],4],[5,[[9,9],0]]]
+        [6,[[[6,2],[5,6]],[[7,6],[4,7]]]]
+        [[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]
+        [[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]
+        [[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]
+        [[[[5,4],[7,7]],8],[[8,3],8]]
+        [[9,3],[[9,9],[6,[4,9]]]]
+        [[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]
+        [[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]
+    "));
 }
 
 fn raw_input() -> &'static str
